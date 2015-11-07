e4295aa6748973c066b4dea34bfeaf63135acc68
Gilbert Chu <gilbertchu91@gmail.com>
2015-11-06 20:40:04 -0800

    scene
    added scene and bare script

---
+++ Assets/Scripts/TimingManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+
+public class TimingManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//Get stored time or initialize at now, then update the display
+	}
+
+	void SetTime() {
+		//Set the starting time
+	}
+
+}
